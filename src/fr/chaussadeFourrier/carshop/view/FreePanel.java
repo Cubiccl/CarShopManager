f9fca922a08e2cdda328a1f65f48857b4a5b7bac
Freecks <leo.fourrier@hotmail.fr>
2016-11-20 17:09:56 +0100

    t('-'t)

--- src/fr/chaussadeFourrier/carshop/view/FreePanel.java
+++ src/fr/chaussadeFourrier/carshop/view/FreePanel.java
@@ -1,14 +1,30 @@
 package fr.chaussadeFourrier.carshop.view;
 
+import java.awt.GridLayout;
+
+
+
 import fr.cubi.cubigui.CPanel;
+import fr.cubi.cubigui.CTextArea;
+import fr.cubi.cubigui.CTextField;
 
 public class FreePanel extends CPanel
 {
 	private static final long serialVersionUID = 2163073256397683674L;
+	private CTextField textField;
+	private CTextArea textArea;
 	
 	public FreePanel()
 	{
 		super("Free SQL");
+		this.setLayout(new GridLayout(2,1));
+		textField = new CTextField();
+		textField.setSize(800,200);
+		textField.setText("test");
+		textArea = new CTextArea("test2");
+		this.add(textField);
+		this.add(textArea);
+		this.setVisible(true);
 	}
 
 }
