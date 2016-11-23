b6740699227fa233c426e740f9829df76c7dd80f
Freecks <leo.fourrier@hotmail.fr>
2016-11-23 19:00:21 +0100

    Ups

--- src/fr/chaussadeFourrier/carshop/view/FreePanel.java
+++ src/fr/chaussadeFourrier/carshop/view/FreePanel.java
@@ -18,6 +18,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
@@ -98,15 +100,22 @@ public class FreePanel extends JSplitPane implements ActionListener,
 		this.setVisible(true);
 	}
 
-	
-	//Save the data in a log file
+	// Save the data in a log file
 	public void saveToFile(String query, ResultSet rs) {
 		try {
-			//Put lines of the log file in a buffer and rewrite them to position pointer
+			// Log file change with date time
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("ddMMyyyy");
+			LocalDate localDate = LocalDate.now();
+			
+			// Put lines of the log file in a buffer and rewrite them to
+			// position pointer
 			String temp;
 			ArrayList<String> lines = new ArrayList<String>();
-			File file = new File("Log.txt");
-			if(!file.exists())
+			File file = new File("Logs/Log" + dtf.format(localDate) + ".txt");
+			File dir = new File("Logs");
+			if(!dir.exists())
+				dir.mkdir();
+			if (!file.exists())
 				file.createNewFile();
 			FileReader fr = new FileReader(file.getAbsoluteFile());
 			BufferedReader br = new BufferedReader(fr);
@@ -120,14 +129,14 @@ public class FreePanel extends JSplitPane implements ActionListener,
 				bw.write(lines.get(i));
 				bw.write(System.getProperty("line.separator"));
 			}
-			
-			//write query
-				bw.write(query);
-				bw.write(System.getProperty("line.separator"));
-				bw.write(System.getProperty("line.separator"));
-			
-			//if query isn't correct, write the error message in the file	
-			if(rs == null){
+
+			// write query
+			bw.write(query);
+			bw.write(System.getProperty("line.separator"));
+			bw.write(System.getProperty("line.separator"));
+
+			// if query isn't correct, write the error message in the file
+			if (rs == null) {
 				bw.write("Erreur : Requête invalide !");
 				bw.write(System.getProperty("line.separator"));
 				bw.write(System.getProperty("line.separator"));
@@ -135,40 +144,80 @@ public class FreePanel extends JSplitPane implements ActionListener,
 				bw.close();
 				return;
 			}
-			
-			//else, write the result
+
+			// else, write the result
 			rs.beforeFirst();
 			ResultSetMetaData rsmd = rs.getMetaData();
+			int x = 0;
+			// check if it's a blob or a big string, different display
 			for (int i = 1; i <= rsmd.getColumnCount(); i++) {
 				bw.write(rsmd.getColumnName(i));
-				for (int j = 0; j < (rsmd.getColumnDisplaySize(i) - rsmd
-						.getColumnName(i).length()); j++)
+				if (rsmd.getColumnTypeName(i) == "BLOB")
+					x = 7;
+				else if (rsmd.getColumnDisplaySize(i) > 100)
+					x = 100;
+				else
+					x = rsmd.getColumnDisplaySize(i);
+				for (int j = 0; j < (x - rsmd.getColumnName(i).length()); j++)
 					bw.write(" ");
 				bw.write("|");
 			}
 			bw.write(System.getProperty("line.separator"));
 			for (int i = 1; i <= rsmd.getColumnCount(); i++) {
-				for (int j = 0; j < Math.max(rsmd.getColumnDisplaySize(i), rsmd
-						.getColumnName(i).length()); j++)
+				if (rsmd.getColumnTypeName(i) == "BLOB")
+					x = 7;
+				else if (rsmd.getColumnDisplaySize(i) > 100)
+					x = 100;
+				else
+					x = rsmd.getColumnDisplaySize(i);
+				for (int j = 0; j < Math.max(x, rsmd.getColumnName(i).length()); j++)
 					bw.write("-");
 				bw.write("+");
 			}
 			bw.write(System.getProperty("line.separator"));
-			int x = 0;
+			String buff = "";
 			while (rs.next()) {
-				for (int i = 1; i <= rsmd.getColumnCount(); i++) {					
-					if (rs.getString(i) == null)
-						x = 0;
-					else{
-						x = rs.getString(i).length();
-						bw.write(rs.getString(i));
+				for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+					if (rsmd.getColumnTypeName(i) == "BLOB") {
+						bw.write("[image]");
+						for (int j = 0; j < Math.max(0, rsmd.getColumnName(i)
+								.length()) - 7; j++)
+							bw.write(" ");
+						bw.write("|");
+					} else if (rs.getString(i)!= null && rs.getString(i).length() > 100) {
+						buff = rs.getString(i).replaceAll("[\\v\\h\\s]"," ");
+						buff = buff.substring(0, 97);
+						bw.write(buff);
+						bw.write("...");
+						bw.write("|");
+					} else if (rsmd.getColumnDisplaySize(i) > 100) {
+						buff = rs.getString(i).replaceAll("[\\v\\h\\s]"," ");
+						buff = rs.getString(i);
+						/*
+						 * In case of some lines contains vertical whitespace which match none of unicode characters spacings referenced on wikipedia
+						 */
+						if(buff.indexOf("Measures approximately 9 1/2\" Long.") != -1){							// Il y a toujours une solution à un problème.
+							buff = buff.substring(0,buff.indexOf("Measures approximately 9 1/2\" Long.")-1);	// t('-'t)
+							buff = buff + "  Measures approximately 9 1/2\" Long.     ";						// t('-'t)
+						}					
+						bw.write(buff);
+						for (int j = 0; j < 100 - buff.length(); j++)
+							bw.write(" ");
+						bw.write("|");
+					} else {
+						if (rs.getString(i) == null)
+							x = 0;
+						else {
+							bw.write(rs.getString(i));
+							x = rs.getString(i).length();
+						}
+						for (int j = 0; j < Math.max(rsmd
+								.getColumnDisplaySize(i), rsmd.getColumnName(i)
+								.length())
+								- x; j++)
+							bw.write(" ");
+						bw.write("|");
 					}
-						
-					for (int j = 0; j < Math.max(rsmd.getColumnDisplaySize(i), rsmd
-							.getColumnName(i).length())
-							- x; j++)
-						bw.write(" ");
-					bw.write("|");
 				}
 				bw.write(System.getProperty("line.separator"));
 			}
@@ -178,9 +227,7 @@ public class FreePanel extends JSplitPane implements ActionListener,
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
-	
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -218,15 +265,15 @@ public class FreePanel extends JSplitPane implements ActionListener,
 			text = text.substring(index);
 			break;
 		}
-		
-		text = text.replace(";"," ");
-		text = text.replaceAll("\n", " ");
-		text = text.replaceAll(" +"," ");				
-		
+
+		if(text.indexOf(";") != -1)
+			text = text.substring(0, text.indexOf(";"));
+		text = text.replaceAll("\\n", " ");
+		text = text.replaceAll(" +", " ");
+
 		textField.setText(text);
 		this.setDividerLocation(88);
-		
-		
+
 		// Connect to database
 		Connection connect = Database.getConnection();
 		try {
@@ -238,14 +285,12 @@ public class FreePanel extends JSplitPane implements ActionListener,
 			String type = "";
 
 			saveToFile(text, rs);
-			
 
 			// For Pie Chart if possible
 
 			if (rsmd.getColumnCount() == 2) {
 				type = rsmd.getColumnTypeName(2);
 			}
-			System.out.println(type);
 			if (type == "INT" || type == "FLOAT" || type == "DOUBLE"
 					|| type == "TINYINT" || type == "MEDIUMINT"
 					|| type == "SMALLINT" || type == "BIGINT"
@@ -255,10 +300,7 @@ public class FreePanel extends JSplitPane implements ActionListener,
 				rs.beforeFirst();
 				DefaultPieDataset pc = new DefaultPieDataset();
 				while (rs.next()) {
-					if(type == "DOUBLE")
 						pc.setValue(rs.getString(1), rs.getDouble(2));
-					if(type == "INT")
-						pc.setValue(rs.getString(1), (double)rs.getInt(2));
 				}
 				JFreeChart pc2 = ChartFactory.createPieChart(
 						rsmd.getColumnName(1), pc, true, true, true);
@@ -281,12 +323,25 @@ public class FreePanel extends JSplitPane implements ActionListener,
 			}
 			for (int i = 0; i < rows; i++) {
 				rs.next();
+				//better display a message telling there's an image than the blob data itself
 				for (int j = 0; j < rsmd.getColumnCount(); j++) {
-					data[i][j] = rs.getString(j + 1);
+					if (rsmd.getColumnTypeName(j + 1) == "BLOB")
+						data[i][j] = "[image]";
+					else
+						data[i][j] = rs.getString(j + 1);
 				}
 			}
 
-			table = new CTable(data, names);
+			table = new CTable(data, names){
+				private static final long serialVersionUID = -6588307240212217769L;
+
+				@Override
+				public boolean isCellEditable(int row, int column)
+				{
+					// prevent cell editing
+					return false;
+				}
+			};
 
 			// Display
 			display.removeAll();
@@ -304,6 +359,7 @@ public class FreePanel extends JSplitPane implements ActionListener,
 
 			// Tells if query isn't correct
 		} catch (SQLException e1) {
+			e1.printStackTrace();
 			JOptionPane.showMessageDialog(null, "Requête invalide !", "Erreur",
 					JOptionPane.ERROR_MESSAGE);
 			saveToFile(text, null);
@@ -323,7 +379,8 @@ public class FreePanel extends JSplitPane implements ActionListener,
 	 */
 	@Override
 	public void keyTyped(KeyEvent e) {
-		if ((table == null || (table != null && table.getRowCount() < 24)) && chart == null)
+		if ((table == null || (table != null && table.getRowCount() < 24))
+				&& chart == null)
 			this.resetToPreferredSizes();
 	}
 
